82c146c60474d1e5078e7a8bb927da330d2cb5e7
slashhuang <627284482@qq.com>
2016-10-09 00:01:35 +0800

    finish rx throttle

--- index.js
+++ index.js
@@ -3,4 +3,30 @@
  */
 import Rx from 'rxjs/Rx';
 
-Rx.Observable.of(1,2,3)
+
+/**
+ * Purity
+
+ What makes RxJS powerful is its ability to produce values using pure functions.
+ That means your code is less prone for errors.
+ * @type {Element}
+ */
+
+var button = document.querySelector('button');
+Rx.Observable.fromEvent(button, 'click')
+    .scan(count => count + 1, 0)
+    .subscribe(count => console.log(`Clicked ${count} times`));
+
+/**
+ * Flow
+
+ RxJS has a whole range of operators that helps you control how the events flow through your observables.
+ * @type {Element}
+ */
+var _button = document.getElementsByClassName('button-flow')[0];
+
+debugger;
+Rx.Observable.fromEvent(_button, 'click')
+    .throttle(1000)
+    .scan(count => count + 1, 0)
+    .subscribe(count => console.log('Clicked ${count} times'));
